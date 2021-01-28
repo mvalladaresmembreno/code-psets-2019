f21f409f658df01741b7ef9dadedb98b84d6ead4
github.com/marstoff <mcvm2101@gmail.com>
2021-01-27 18:11:11 -0600

    PSet8

---
+++ pset8/mashup/bbd.sql
@@ -0,0 +1,13 @@
+create table places(
+country_code text,
+postal_code text,
+place_name text,
+admin_name1 text,
+admin_code1 text,
+admin_name2 text,
+admin_code2 text,
+admin_name3 text,
+admin_code3 text,
+latitude numeric,
+longitude numeric
+accuracy integer);
